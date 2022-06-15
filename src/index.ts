2b24da4b1067dae736e18b84fef6272acbf753ad
U-Jinyeol <100745412+U-Jinyeol@users.noreply.github.com>
2022-06-15 15:02:05 +0900

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+// Function Overloads
+
+// 동일한 이름에 매개 변수와 매개 변수 타입 또는 리턴 타입이 다른 여러 버전의 함수를 만드는 것
+// TypeScript에서는 오버로드 signatures을 작성하여 "다양한 방식으로 호출할 수 있는 함수"를 지정할 수 있음
+
+type Add = {
+  (a: number, b: number): number;
+  (a: number, b: number, c: number): number;
+};
+
+const add: Add = (a, b, c?: number) => {
+  if (c) return a + b + c;
+  return a + b;
+};
+
+add(1, 2);
+add(1, 2, 3);
